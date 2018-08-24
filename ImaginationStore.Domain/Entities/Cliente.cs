1cee9d9ca6ce604e3dd1ca888093715859ed9650
JuanDiasV <33902980+JuanDiasV@users.noreply.github.com>
2018-08-24 11:47:53 -0300

    Mapping feitos

--- ImaginationStore.Domain/Entities/Cliente.cs
+++ ImaginationStore.Domain/Entities/Cliente.cs
@@ -5,7 +5,7 @@
         public virtual int Codigo { get; protected set; }
         public virtual string Nome { get; protected set; }
         public virtual int Telefone { get; protected set; }
-        public virtual string Filizacao { get; protected set; }
+        public virtual string Filiacao { get; protected set; }
         public virtual int Status { get; protected set; }
         public virtual Endereco Endereco { get; protected set; }
         public virtual decimal LimiteDeCredito { get; protected set; }
@@ -13,12 +13,12 @@
         //Instancia para poder realziar conexão
         public Cliente() { }
 
-        public Cliente(int codigo, string nome, int telefone, string filizacao, int status, Endereco endereco, decimal limitedecredito)
+        public Cliente(int codigo, string nome, int telefone, string filiacao, int status, Endereco endereco, decimal limitedecredito)
         {
             this.Codigo = codigo;
             this.Nome = nome;
             this.Telefone = telefone;
-            this.Filizacao = filizacao;
+            this.Filiacao = filiacao;
             this.Status = status;
             this.Endereco = endereco;
             this.LimiteDeCredito = limitedecredito;
