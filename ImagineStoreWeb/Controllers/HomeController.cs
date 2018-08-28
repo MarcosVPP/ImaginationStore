f0869a3d699a1452732a9d170b71a6e6f0e2c385
JuanDiasV <33902980+JuanDiasV@users.noreply.github.com>
2018-08-28 11:06:05 -0300

    Vendo o pull

--- ImagineStoreWeb/Controllers/HomeController.cs
+++ ImagineStoreWeb/Controllers/HomeController.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Web;
 using ImaginationStore.Domain.Entities;
 using System.Web.Mvc;
+using ImaginationStore.Integration.Repositories;
 
 namespace ImagineStoreWeb.Controllers
 {
     public class HomeController : Controller
-    {        
+    {
+        int id = 2;
 
         public ActionResult Menu()
         {
+           
+            ViewBag.BuscarCategoria();
+
             return View();
+        }
+
+        //Classe para buscar Categoria por ID somente
+        void BuscarCategoria()
+        {
+           Console.WriteLine(CategoriaRepository.BuscaProdutoPorCodigo(id).Nome);
+           
         }
 
     }
