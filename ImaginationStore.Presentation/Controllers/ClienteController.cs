ea9ae1509e0fc1117b9ba6b0df60188922578063
Marcos Pereira <marcosppereira16@hotmail.com>
2018-08-30 12:52:58 -0300

    quem sabe

--- ImaginationStore.Presentation/Controllers/ClienteController.cs
+++ ImaginationStore.Presentation/Controllers/ClienteController.cs
@@ -19,11 +19,7 @@ namespace ImaginationStore.Presentation.Controllers
 
         // GET: Cliente
         [HttpPost]
-<<<<<<< HEAD
         public ActionResult Get(/*ClienteService salvarcliente*/)
-=======
-        public ActionResult Index()
->>>>>>> 9453e3d09f49fcf43e77335d0623d313f98cb0a5
         {
             //object test;
             //Tentando chamar o metodo para salvar cliente
@@ -110,13 +106,7 @@ namespace ImaginationStore.Presentation.Controllers
             {
                 return View();
             }
-<<<<<<< HEAD
-
         }
-
-
-=======
         }
->>>>>>> 9453e3d09f49fcf43e77335d0623d313f98cb0a5
     }
-}
+
